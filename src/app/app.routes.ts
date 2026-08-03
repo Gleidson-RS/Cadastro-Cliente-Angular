7f7615c9ecfbca1ef781fe1c5e417c170449bbe1
Gleidson <gleidsonrosasantos2@gmail.com>
2026-08-03 14:22:22 -0300

    app.component.html configurado com <router-outlet></router-outlet>, e app.routes.ts com import { Routes } from '@angular/router'; import { 'classe importada' } from './caminho da classe importada';
    export const routes: Routes = [{
    
        path:'',
        component: 'classe importada'
    }];

---
+++ src/app/app.routes.ts
@@ -0,0 +1,8 @@
+import { Routes } from '@angular/router';
+import { CadastroClienteComponent } from './cadastro-cliente/cadastro-cliente.component';
+
+export const routes: Routes = [{
+
+    path:'',
+    component:CadastroClienteComponent
+}];
